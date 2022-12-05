f2ecf5d26f34f78c1c89aa1ff34bcdb0716be401
Matheus Oliveira <107084202+MatheusOliveira08@users.noreply.github.com>
2022-12-05 17:43:58 -0300

    create: Lista de vídeos na memória
    Criando uma lista de vídeos para mostrar na Home Page

--- hello-world/app/Controllers/Http/VideosController.ts
+++ hello-world/app/Controllers/Http/VideosController.ts
@@ -1,11 +1,22 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class VideosController {
+    public static videos = {
+        size: 3,
+        values: {
+            1: {id: 0, nome: 'Aula 01:', description: 'Introdução Web'},
+            2: {id: 2, nome: 'Aula 02:', description: 'Introdução ao HTTP'},
+            3: {id: 1, nome: 'Aula 03:', description: 'Introdução HTML'}
+        }
+    }
+
     public async home({ response }: HttpContextContract){
         return response.redirect().toRoute('videos.index')
     }
 
     public async index({ view }: HttpContextContract){
-        return view.render('videos/index')
+        const videos = VideosController.videos.values
+
+        return view.render('videos/index', { videos: videos})
     }
 }
